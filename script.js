5fb26253fd35287d0f86bb80de7efde175648161
saarahah <sarahlferguson07@gmail.com>
2020-06-23 23:54:25 -0400

    working search button

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 $(document).ready(function(){
 
+// var searchForm = document.querySelector(".form-inline");
+// var searchBtn = document.querySelector(".btn");
+// var formControl=document.querySelector(".form-control");
+
+var container = $(".container");
+var searchForm = $(".form-inline");
+var formControl = $(".form-control")
+var searchBtn = $(".btn")
+
 var APIKey = "be6a5afd637e52c8345f9d5c594e2f10";
 
 var location = "Orlando"
@@ -21,5 +30,31 @@ var location = "Orlando"
         // Log the resulting object
         console.log(response);
     })
+
+    // searchBtn.addEventListener("click", function(event) {
+    //      event.preventDefault();
+    //   console.log("this is form control" + formControl.sibling)
+    //    console.log("this is search form" + searchForm.nextSibling);
+
+    // $(document).on("click", searchBtn, function(e) {  
+    //      alert("saved!");
+    //     event.preventDefault();
+    //     var input = $(this).closest(formControl).val();
+    //     console.log($(this).closest(formControl).val());
+
+
+    $(searchBtn).on("click", function(e){
+        alert("The paragraph was clicked.");
+        event.preventDefault();
+        var input = $(this).siblings(formControl).val();
+       console.log($(this).siblings(formControl).val());
+      });
+
+
+    //   $(document).on("click", ".saveBtn", function(e) {  
+    //     alert("saved!");
+    //     var input = $(this).siblings("textarea").val();
+        
+ 
 
 });
